4922522c86068859d9b4da57bc715e5092562330
Donat Marko <marko.donat@gmail.com>
2019-12-26 11:59:44 +0100

    :ambulance: :art: Fixes double rows when the same user reconnects. Adds statistics.

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ logger.warning('IVAO Tracker Backend has been started.')
 config = configparser.ConfigParser()
 config.read('ivao-tracker.ini')
 logger.info('Config loaded.')
-
+'''
 def error_handler(type, value, tb):
     logger.exception(value)
     sys.exit(1)
-sys.excepthook = error_handler
+sys.excepthook = error_handler'''
 
 '''
     * SQL szerint onlineok legyűjtése tömbbe, mindegyik elem action flag := not_updated
@@ -48,18 +48,18 @@ def track():
     # legyűjtjük az SQL szerint online klienseket (köztük lehet már nem online is)
     logger.info('Getting ATCs from SQL...')
     cursor.execute('SELECT * FROM atcs WHERE online = 1 ORDER BY callsign')
-    atcs = {}
+    atcs = []
     for atc in cursor.fetchall():
         atc['_action'] = 'not_updated'
-        atcs[atc['callsign']] = atc
+        atcs.append(atc)
         
     # meg a pilótákat is
     logger.info('Getting pilots from SQL...')
     cursor.execute('SELECT * FROM pilots WHERE online = 1 ORDER BY callsign')
-    pilots = {}
+    pilots = []
     for pilot in cursor.fetchall():
         pilot['_action'] = 'not_updated'
-        pilots[pilot['callsign']] = pilot
+        pilots.append(pilot)
     
     
     # legyűjtjük a whazzup szerint online klienseket (ők a biztosan online-ok)
@@ -74,183 +74,188 @@ def track():
             in_sql = 0
             
             # erre azért van szükség, mert lehetnek a whazzupban broken sorok (software = 2 6 pl.), ezeknél a rating mindig üres. ezek nem kellenek
-            if len(wzrow['rating']) > 1:
-                # megtalálható-e az SQL-ben egyező VID-del
-                if wzrow['callsign'] in atcs:
-                    atc = atcs[wzrow['callsign']]
-                    
-                    if wzrow['vid'] == atc['vid'] and wzrow['connected_at'] == atc['connected_at'].strftime('%Y%m%d%H%M%S') and wzrow['software'] == atc['software']:
+            if len(wzrow['rating']) > 1:           
+                # megkeressük a tömbben az azonos callsignnal rendelkező sort
+                idx = next((index for (index, d) in enumerate(atcs) if d["callsign"] == wzrow['callsign']), -1)
+                
+                if idx > -1:
+                    if wzrow['vid'] == atcs[idx]['vid'] and wzrow['connected_at'] == atcs[idx]['connected_at'].strftime('%Y%m%d%H%M%S') and wzrow['software'] == atcs[idx]['software']:
                         in_sql = 1
                 
                 if in_sql:
                     logger.debug('ATC %s is already online in SQL, will be updated.' % (wzrow['callsign']))
-                    id = atc['id']
+                    id = atcs[idx]['id']
                     atc = wzrow.copy()
                     atc['id'] = id
                     atc['_action'] = 'updated'
-                    atcs[wzrow['callsign']] = atc
+                    atcs[idx] = atc
                 else:
                     logger.debug('ATC %s is not in SQL, will be added.' % (wzrow['callsign']))
                     atc = wzrow.copy()
                     atc['_action'] = 'created'
-                    atcs[wzrow['callsign']] = atc
+                    atcs.append(atc)
+            else:
+                logger.warning('Broken whazzup line (%s) found, skipping.' % (wzrow['callsign']))
                     
                     
         for wzrow in wz_pilots:
             in_sql = 0
             
             # erre azért van szükség, mert lehetnek a whazzupban broken sorok (software = 2 6 pl.), ezeknél a rating mindig üres. ezek nem kellenek
-            if len(wzrow['rating']) > 1:
-                # megtalálható-e az SQL-ben egyező VID-del
-                if wzrow['callsign'] in pilots:
-                    pilot = pilots[wzrow['callsign']]
-                    
-                    if wzrow['vid'] == pilot['vid'] and wzrow['connected_at'] == pilot['connected_at'].strftime('%Y%m%d%H%M%S') and wzrow['software'] == pilot['software']:
+            if len(wzrow['rating']) > 1:           
+                # megkeressük a tömbben az azonos callsignnal rendelkező sort
+                idx = next((index for (index, d) in enumerate(pilots) if d["callsign"] == wzrow['callsign']), -1)
+                
+                if idx > -1:
+                    if wzrow['vid'] == pilots[idx]['vid'] and wzrow['connected_at'] == pilots[idx]['connected_at'].strftime('%Y%m%d%H%M%S') and wzrow['software'] == pilots[idx]['software']:
                         in_sql = 1
                 
                 if in_sql:
                     logger.debug('Flight %s is already online in SQL, will be updated.' % (wzrow['callsign']))
-                    id = pilot['id']
+                    id = pilots[idx]['id']
                     pilot = wzrow.copy()
                     pilot['id'] = id
                     pilot['_action'] = 'updated'
-                    pilots[wzrow['callsign']] = pilot
+                    pilots[idx] = pilot
                 else:
                     logger.debug('Flight %s is not in SQL, will be added.' % (wzrow['callsign']))
                     pilot = wzrow.copy()
                     pilot['_action'] = 'created'
-                    pilots[wzrow['callsign']] = pilot
+                    pilots.append(pilot)
+            else:
+                logger.warning('Broken whazzup line (%s) found, skipping.' % (wzrow['callsign']))
                 
     else:
         logging.error('Status code: %s' % r.status_code)
         
     
+    atc_created = 0
+    atc_updated = 0
+    atc_deleted = 0
     for atc in atcs:
-        if atcs[atc]['_action'] == 'not_updated':
-            atcs[atc]['_action'] = 'deleted'
-            logger.debug('ATC %s is not in Whazzup, will be deleted.' % (atc))
-            
-        if atcs[atc]['_action'] == 'updated':
-            a = atcs[atc]
+        if atc['_action'] == 'updated':
             cursor.execute('UPDATE atcs SET latitude = %s, longitude = %s, frequency = %s, radar_range = %s, atis = %s, atis_time = %s, last_tracked_at = NOW() WHERE id = %s', (
-                a['latitude'],
-                a['longitude'],
-                a['frequency'],
-                a['radar_range'],
-                a['atis'],
-                a['atis_time'],
-                a['id'],
+                atc['latitude'],
+                atc['longitude'],
+                atc['frequency'],
+                atc['radar_range'],
+                atc['atis'],
+                atc['atis_time'],
+                atc['id'],
             ))
-            logger.debug('ATC %s has been updated in SQL.' % (atc))
+            logger.debug('ATC session #%s (%s) has been updated in SQL.' % (atc['id'], atc['callsign']))
+            atc_updated += 1
         
-        if atcs[atc]['_action'] == 'created':
-            a = atcs[atc]
+        if atc['_action'] == 'created':
             cursor.execute('INSERT INTO atcs (callsign, vid, status, rating, latitude, longitude, server, protocol, software, frequency, radar_range, atis, atis_time, online, connected_at, last_tracked_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, %s, NOW())', (
-                a['callsign'],
-                a['vid'],
-                a['status'],
-                a['rating'],
-                a['latitude'],
-                a['longitude'],
-                a['server'],
-                a['protocol'],
-                a['software'],
-                a['frequency'],
-                a['radar_range'],
-                a['atis'],
-                a['atis_time'],
-                a['connected_at'],
+                atc['callsign'],
+                atc['vid'],
+                atc['status'],
+                atc['rating'],
+                atc['latitude'],
+                atc['longitude'],
+                atc['server'],
+                atc['protocol'],
+                atc['software'],
+                atc['frequency'],
+                atc['radar_range'],
+                atc['atis'],
+                atc['atis_time'],
+                atc['connected_at'],
             ))
-            logger.debug('ATC %s has been added to SQL.' % (atc))
+            logger.debug('New ATC session (%s) has been added to SQL.' % (atc['callsign']))
+            atc_created += 1
             
-        if atcs[atc]['_action'] == 'deleted':
-            a = atcs[atc]
-            cursor.execute('UPDATE atcs SET online = 0, disconnected_at = NOW() WHERE id = %s', (a['id'], ))
-            logger.debug('ATC %s has been deleted from SQL.' % (atc))
+        if atc['_action'] == 'not_updated':
+            cursor.execute('UPDATE atcs SET online = 0, disconnected_at = NOW() WHERE id = %s', (atc['id'], ))
+            logger.debug('ATC session #%s (%s) has been deleted from SQL.' % (atc['id'], atc['callsign']))
+            atc_deleted += 1
     
     
+    pilot_created = 0
+    pilot_updated = 0
+    pilot_deleted = 0
     for pilot in pilots:
-        if pilots[pilot]['_action'] == 'not_updated':
-            pilots[pilot]['_action'] = 'deleted'
-            logger.debug('Flight %s is not in Whazzup, will be deleted.' % (pilot))
-            
-        if pilots[pilot]['_action'] == 'updated':
-            a = pilots[pilot]
+        if pilot['_action'] == 'updated':
             cursor.execute('UPDATE pilots SET latitude = %s, longitude = %s, heading = %s, on_ground = %s, altitude = %s, groundspeed = %s, mode_a = %s, fp_aircraft = %s, fp_speed = %s, fp_rfl = %s, fp_departure = %s, fp_destination = %s, fp_alternate = %s, fp_alternate2 = %s, fp_type = %s, fp_pob = %s, fp_route = %s, fp_item18 = %s, fp_rev = %s, fp_rule = %s, fp_deptime = %s, fp_eet = %s, fp_endurance = %s, last_tracked_at = NOW() WHERE id = %s', (
-                a['latitude'],
-                a['longitude'],
-                a['heading'],
-                a['on_ground'],
-                a['altitude'],
-                a['groundspeed'],
-                a['mode_a'],
-                a['fp_aircraft'],
-                a['fp_speed'],
-                a['fp_rfl'],
-                a['fp_departure'],
-                a['fp_destination'],
-                a['fp_alternate'],
-                a['fp_alternate2'],
-                a['fp_type'],
-                a['fp_pob'],
-                a['fp_route'],
-                a['fp_item18'],
-                a['fp_rev'],
-                a['fp_rule'],
-                a['fp_deptime'],
-                a['fp_eet'],
-                a['fp_endurance'],
-                a['id'],
+                pilot['latitude'],
+                pilot['longitude'],
+                pilot['heading'],
+                pilot['on_ground'],
+                pilot['altitude'],
+                pilot['groundspeed'],
+                pilot['mode_a'],
+                pilot['fp_aircraft'],
+                pilot['fp_speed'],
+                pilot['fp_rfl'],
+                pilot['fp_departure'],
+                pilot['fp_destination'],
+                pilot['fp_alternate'],
+                pilot['fp_alternate2'],
+                pilot['fp_type'],
+                pilot['fp_pob'],
+                pilot['fp_route'],
+                pilot['fp_item18'],
+                pilot['fp_rev'],
+                pilot['fp_rule'],
+                pilot['fp_deptime'],
+                pilot['fp_eet'],
+                pilot['fp_endurance'],
+                pilot['id'],
             ))
-            logger.debug('Flight %s has been updated in SQL.' % (pilot))
+            logger.debug('Flight session #%s (%s) has been updated in SQL.' % (pilot['id'], pilot['callsign']))
+            pilot_updated += 1
         
-        if pilots[pilot]['_action'] == 'created':
-            a = pilots[pilot]
+        if pilot['_action'] == 'created':
+            a = pilot
             cursor.execute('INSERT INTO pilots (callsign, vid, status, rating, latitude, longitude, server, protocol, software, heading, on_ground, altitude, groundspeed, mode_a, fp_aircraft, fp_speed, fp_rfl, fp_departure, fp_destination, fp_alternate, fp_alternate2, fp_type, fp_pob, fp_route, fp_item18, fp_rev, fp_rule, fp_deptime, fp_eet, fp_endurance, sim_type, online, connected_at, last_tracked_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, %s, NOW())', (
-                a['callsign'],
-                a['vid'],
-                a['status'],
-                a['rating'],
-                a['latitude'],
-                a['longitude'],
-                a['server'],
-                a['protocol'],
-                a['software'],
-                a['heading'],
-                a['on_ground'],
-                a['altitude'],
-                a['groundspeed'],
-                a['mode_a'],
-                a['fp_aircraft'],
-                a['fp_speed'],
-                a['fp_rfl'],
-                a['fp_departure'],
-                a['fp_destination'],
-                a['fp_alternate'],
-                a['fp_alternate2'],
-                a['fp_type'],
-                a['fp_pob'],
-                a['fp_route'],
-                a['fp_item18'],
-                a['fp_rev'],
-                a['fp_rule'],
-                a['fp_deptime'],
-                a['fp_eet'],
-                a['fp_endurance'],
-                a['sim_type'],
-                a['connected_at'],
+                pilot['callsign'],
+                pilot['vid'],
+                pilot['status'],
+                pilot['rating'],
+                pilot['latitude'],
+                pilot['longitude'],
+                pilot['server'],
+                pilot['protocol'],
+                pilot['software'],
+                pilot['heading'],
+                pilot['on_ground'],
+                pilot['altitude'],
+                pilot['groundspeed'],
+                pilot['mode_a'],
+                pilot['fp_aircraft'],
+                pilot['fp_speed'],
+                pilot['fp_rfl'],
+                pilot['fp_departure'],
+                pilot['fp_destination'],
+                pilot['fp_alternate'],
+                pilot['fp_alternate2'],
+                pilot['fp_type'],
+                pilot['fp_pob'],
+                pilot['fp_route'],
+                pilot['fp_item18'],
+                pilot['fp_rev'],
+                pilot['fp_rule'],
+                pilot['fp_deptime'],
+                pilot['fp_eet'],
+                pilot['fp_endurance'],
+                pilot['sim_type'],
+                pilot['connected_at'],
             ))
-            logger.debug('Flight %s has been added to SQL.' % (pilot))
+            logger.debug('New flight session (%s) has been added to SQL.' % (pilot['callsign']))
+            pilot_created += 1
             
-        if pilots[pilot]['_action'] == 'deleted':
-            a = pilots[pilot]
-            cursor.execute('UPDATE pilots SET online = 0, disconnected_at = NOW() WHERE id = %s', (a['id'], ))
-            logger.debug('Flight %s has been deleted from SQL.' % (pilot))
-            
+        if pilot['_action'] == 'not_updated':
+            cursor.execute('UPDATE pilots SET online = 0, disconnected_at = NOW() WHERE id = %s', (pilot['id'], ))
+            logger.debug('Flight session #%s (%s) has been deleted from SQL.' % (pilot['id'], pilot['callsign']))
+            pilot_deleted += 1
+           
     db.commit()
     cursor.close()
     db.close()
     logger.info('Tracking is done, DB has been committed.')
+    logger.info('ATC statistics: %s created, %s updated, %s deleted. Currently %s online.' % (atc_created, atc_updated, atc_deleted, (atc_created + atc_updated)))
+    logger.info('Pilot statistics: %s created, %s updated, %s deleted. Currently %s online.' % (pilot_created, pilot_updated, pilot_deleted, (pilot_created + pilot_updated)))
 
 
 track()
